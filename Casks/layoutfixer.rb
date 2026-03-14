c679403dc5110ed4cf8c626cf0696d8b2dc566b7
potapyich <potapyich@gmail.com>
2026-03-14 19:15:05 +0100

    Update layoutfixer version to 1.0.8

--- Casks/layoutfixer.rb
+++ Casks/layoutfixer.rb
@@ -1,6 +1,6 @@
 cask "layoutfixer" do
-  version "1.0.7"
-  sha256 "f08cf534c520c6750d7ace0e1a7c8583412150d649a43526d3c1f0a57b696278"
+  version "1.0.8"
+  sha256 "2d9284d3b9de4c8b81b47e9e67f6685567f15a2ec3dcb9a991d8559c5a992b50"
 
   url "https://github.com/potapyich/LayoutFixer/releases/download/v#{version}/LayoutFixer.zip"
   name "LayoutFixer"

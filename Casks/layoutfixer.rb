7314b807ca5ed83f83dd2dad133dfc04179eaa56
Mikhail Pakatsilau <potapyich@gmail.com>
2026-04-30 22:59:26 +0200

    layoutfixer 1.0.18

--- Casks/layoutfixer.rb
+++ Casks/layoutfixer.rb
@@ -1,6 +1,6 @@
 cask "layoutfixer" do
-  version "1.0.16"
-  sha256 "99c46b7314af6e6ca4105d7c4665934b033c72acf68ea6f58a7bc61a69af2e25"
+  version "1.0.18"
+  sha256 "78ad8f921092a0cc1dba5941ce6f7fb778082ce8386b957344109fe5de77e6a9"
 
   url "https://github.com/potapyich/LayoutFixer/releases/download/v#{version}/LayoutFixer.zip"
   name "LayoutFixer"
